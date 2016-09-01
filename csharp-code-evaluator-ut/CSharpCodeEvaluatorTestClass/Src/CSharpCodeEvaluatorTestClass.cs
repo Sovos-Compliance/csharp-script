d08dd7a2843bc058db24e45c45f8f238d7076084
Jose Sebastian Battig <jsbattig@convey.com>
2016-08-31 21:26:37 -0500

    B-0 moved some files around

---
+++ csharp-code-evaluator-ut/CSharpCodeEvaluatorTestClass/Src/CSharpCodeEvaluatorTestClass.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpCodeEvaluatorTestClass
+{
+  // ReSharper disable once InconsistentNaming
+  public class TestClassInGACAssembly
+  {
+    public int a;
+
+    public TestClassInGACAssembly()
+    {
+      a = 10;
+    }
+
+    public int Inc()
+    {
+      return ++a;
+    }
+  }
+}
