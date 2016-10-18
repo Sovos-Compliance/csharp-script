4a4db2c3acc496c3e1d7932a327e4f8d96ccb495
Jose Sebastian Battig <jsbattig@convey.com>
2016-10-18 09:52:55 -0500

    B-0 removed ICSharpScriptException. Use new method invoker interface to access methods declared in the class

--- src/CSharpScriptException.cs
+++ src/CSharpScriptException.cs
@@ -2,17 +2,20 @@ using System;
 
 namespace Sovos.Scripting.CSharpScriptObjectBase
 {
-  public interface ICSharpScriptException
-  {
-    void SetMessage(string message);
-  }
-
-  public class CSharpScriptException : Exception, ICSharpScriptException
+  public class CSharpScriptException : Exception, ICSharpScriptObjectMethodInvoker
   {
     private string _message = "";
 
     public CSharpScriptException(){}
     public CSharpScriptException(string msg) : base(msg){}
+
+    public object Invoke(string methodName, object[] args)
+    {
+      var method = GetType().GetMethod(methodName);
+      if (method != null)
+        return method.Invoke(this, args);
+      throw new MissingMethodException(string.Format("Method \"{0}\"not found", methodName));
+    }
 
     public override string Message
     {
